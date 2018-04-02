a7c312253f6bd577ecb9ceca031360d103bc6f6d
1833041 <1833041@00-67054.pusd.dom>
2018-04-02 11:53:44 -0700

    whats even happening

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,10 @@
 
 public class Test {
-	public static void main(String args[])
-	{
-<<<<<<< HEAD
-	System.out.println("Hello Worljjd!");
-=======
+	public static void main(String args[]) {
+
+		System.out.println("Hello Worljjd!");
+
 		System.out.println("Hello World!");
 		int lol = 1;
->>>>>>> branch 'master' of https://github.com/Jakbquick/JavaExcercises.git
 	}
-}	
+}
