ae520c8297ed24b0698754f0b2f146a9d9244a2d
John Manard <jakbquick2000@gmail.com>
2018-04-03 21:03:05 -0700

    Finished all labs from Think Java Lesson 7

--- Chapter7/IterativeFactorial.java
+++ Chapter7/IterativeFactorial.java
@@ -1,7 +1,13 @@
 
 public class IterativeFactorial {
 	public static void main(String args[]) {
-		System.out.println(factorial(3));
+		for (double i = .1; i <= 100; i *= 10) {
+			System.out.println(outputResult(i));
+		}
+		System.out.println("");
+		for (double i = -.1; i >= -100; i *= 10) {
+			System.out.println(outputResult(i));
+		}
 	}
 
 	public static int factorial(int num) {
@@ -10,5 +16,19 @@ public class IterativeFactorial {
 			product *= i;
 		}
 		return product;
+	}
+
+	public static double myExp(double x, int n) {
+		double sum = 0;
+		double term = 1;
+		for (int i = 1; i <= n; i++) {
+			sum += term;
+			term = (term * x) / (i);
+		}
+		return sum;
+	}
+
+	public static String outputResult(double x) {
+		return "" + x + "\t" + myExp(x, 200) + "\t" + Math.exp(x);
 	}
 }
